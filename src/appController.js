7054779d797617ce318070e129de280883e0c110
maziyard <maziyard@students.cs.ubc.ca>
2023-11-13 19:03:56 -0500

    Maz-intial commits to the civialn start page.

--- src/appController.js
+++ src/appController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const appService = require('./appService');
 
+
 const router = express.Router();
 
 // ----------------------------------------------------------
@@ -28,6 +29,15 @@ router.post("/initiate-demotable", async (req, res) => {
         res.status(500).json({ success: false });
     }
 });
+router.post('/civilian/incident', async (req, res) => {
+    try {
+        // Your logic to handle the request...
+    } catch (error) {
+        console.error('Error processing request:', error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
 
 router.post("/insert-demotable", async (req, res) => {
     const { id, name } = req.body;
@@ -36,6 +46,106 @@ router.post("/insert-demotable", async (req, res) => {
         res.json({ success: true });
     } else {
         res.status(500).json({ success: false });
+    }
+});
+
+router.post('/civilian/incident', async (req, res) => {
+    // Extract relevant data from request body
+    const { Email, Description, Date, Status, Involved } = req.body;
+    // Construct incident data based on the request
+    const incidentData = {
+        Email,
+        Description,
+        Date: Date || new Date().toISOString(),
+        Status: Status || 'created',
+        Involved
+    };
+
+    try {
+        const responseData = await reportIncident(incidentData);
+        if (responseData) {
+            res.status(200).json(responseData);
+        } else {
+            res.status(500).json({ message: 'Failed to report the incident' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error reporting the incident', error: error.message });
+    }
+});
+
+router.post('/incident-update', async (req, res) => {
+    const { incidentID, newDescription, date } = req.body;
+    try {
+        const updateResult = await appService.updateIncident(incidentID, newDescription, date);
+        if (updateResult) {
+            res.json({ success: true });
+        } else {
+            res.status(500).json({ success: false });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error updating the incident', error: error.message });
+    }
+});
+
+router.delete('/incident', async (req, res) => {
+    const { incidentID } = req.query;
+    try {
+        const deleteResult = await appService.deleteIncident(incidentID);
+        if (deleteResult) {
+            res.json({ success: true });
+        } else {
+            res.status(500).json({ success: false });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error deleting the incident', error: error.message });
+    }
+});
+
+router.get('/reporter', async (req, res) => {
+    const { email } = req.query;
+    try {
+        const reporterDetails = await appService.getReporterDetails(email);
+        if (reporterDetails) {
+            res.json(reporterDetails);
+        } else {
+            res.status(404).json({ message: 'Reporter not found' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error retrieving reporter details', error: error.message });
+    }
+});
+
+router.put('/reporter', async (req, res) => {
+    const { name, address, phoneNumber, email } = req.body;
+    try {
+        const updateResult = await appService.updateReporterDetails(name, address, phoneNumber, email);
+        if (updateResult) {
+            res.json({ success: true });
+        } else {
+            res.status(500).json({ success: false });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Error updating reporter details', error: error.message });
+    }
+});
+
+router.get('/municipal/incident', async (req, res) => {
+    const { sort_by, status, display } = req.query;
+    try {
+        const incidents = await appService.getIncidents(sort_by, status, display ? display.split(',') : []);
+        res.json(incidents);
+    } catch (error) {
+        res.status(500).json({ message: 'Error retrieving incidents', error: error.message });
+    }
+});
+
+router.get('/municipal/equipment', async (req, res) => {
+    const { incidentID, sort_by, display } = req.query;
+    try {
+        const equipmentDetails = await appService.getEquipmentDetails(incidentID, sort_by, display ? display.split(',') : []);
+        res.json(equipmentDetails);
+    } catch (error) {
+        res.status(500).json({ message: 'Error retrieving equipment details', error: error.message });
     }
 });
 
--- src/appService.js
+++ src/appService.js
@@ -1,6 +1,6 @@
 const oracledb = require('oracledb');
 const loadEnvFile = require('./utils/envUtil');
-
+const fetch = require('node-fetch');
 const envVariables = loadEnvFile('./.env');
 
 // Database configuration setup. Ensure your .env file has the required database credentials.
@@ -72,6 +72,203 @@ async function initiateDemotable() {
         return false;
     });
 }
+
+// Define the incident data according to your ER diagram and functionality requirements
+const incidentData = {
+    Email: 'user@example.com',
+    Description: 'Description of the incident',
+    Date: new Date().toISOString(),
+    Status: 'created',
+    Involved: [
+      {
+        name: 'John Doe',
+        specialAttributes: { /* special attributes as per your ER diagram */ }
+      },
+      // ... other involved persons
+    ]
+  };
+  
+  // Function to make the POST request to the server
+  async function reportIncident(incidentData) {
+    const url = 'http://localhost:65535/civilian/incident'; // Replace with your actual domain and port
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                // Add other headers like authorization if required
+            },
+            body: JSON.stringify(incidentData) // Convert the incident data to a JSON string
+        });
+
+        if (response.ok) {
+            const responseData = await response.json();
+            console.log('Incident reported successfully. Incident ID:', responseData.incidentID);
+            return responseData; // Contains the incidentID
+        } else {
+            console.error('Failed to report the incident. Status:', response.status);
+            return null;
+        }
+    } catch (error) {
+        console.error('Error reporting the incident:', error);
+        return null;
+    }
+}
+  
+  // Call the function with the incident data
+  reportIncident(incidentData).then(responseData => {
+    if (responseData) {
+      // Do something with the responseData.incidentID
+    }
+  });
+
+
+  async function updateIncident(incidentID, newDescription, date) {
+    const updateData = {
+      incidentID,
+      date,
+      newDescription,
+      Status: 'Cancelled' // Status is hardcoded as "Cancelled"
+    };
+  
+    try {
+      const response = await fetch('/incident-update', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          // Include any other headers like authorization tokens if required
+        },
+        body: JSON.stringify(updateData)
+      });
+  
+      if (response.ok) {
+        console.log('Incident updated successfully.');
+      } else {
+        console.error('Failed to update the incident. Status:', response.status);
+      }
+    } catch (error) {
+      console.error('Error updating the incident:', error);
+    }
+  }
+
+  async function deleteIncident(incidentID) {
+    try {
+      const response = await fetch(`/incident?incidentID=${encodeURIComponent(incidentID)}`, {
+        method: 'DELETE',
+        // Include headers if necessary, like authorization
+      });
+  
+      if (response.ok) {
+        console.log('Incident deleted successfully.');
+      } else {
+        console.error('Failed to delete the incident. Status:', response.status);
+      }
+    } catch (error) {
+      console.error('Error deleting the incident:', error);
+    }
+  }
+  
+  async function getReporterDetails(email) {
+    try {
+      const response = await fetch(`/reporter?email=${encodeURIComponent(email)}`, {
+        method: 'GET',
+        // Include headers if necessary
+      });
+  
+      if (response.ok) {
+        const reporterDetails = await response.json();
+        console.log('Reporter details:', reporterDetails);
+        return reporterDetails;
+      } else if (response.status === 404) {
+        console.error('Reporter not found.');
+      } else {
+        console.error('Failed to get reporter details. Status:', response.status);
+      }
+    } catch (error) {
+      console.error('Error getting reporter details:', error);
+    }
+  }
+  
+  
+  async function updateReporterDetails(name, address, phoneNumber, email) {
+    const reporterData = {
+      Name: name,
+      Address: address,
+      PhoneNumber: phoneNumber,
+      Email: email
+    };
+  
+    try {
+      const response = await fetch('/reporter', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          // Include any other headers like authorization tokens if required
+        },
+        body: JSON.stringify(reporterData)
+      });
+  
+      if (response.ok) {
+        console.log('Reporter details updated successfully.');
+      } else {
+        console.error('Failed to update reporter details. Status:', response.status);
+      }
+    } catch (error) {
+      console.error('Error updating reporter details:', error);
+    }
+  }
+
+  
+  async function getIncidents(sort_by, status, display) {
+    const queryParams = new URLSearchParams({
+      sort_by,
+      status,
+      display: display.join(',') // Convert array to comma-separated string
+    });
+  
+    try {
+      const response = await fetch(`/municipal/incident?${queryParams}`, {
+        method: 'GET',
+        // Include headers if necessary, such as authorization tokens
+      });
+  
+      if (response.ok) {
+        const incidents = await response.json();
+        console.log('Incidents retrieved:', incidents);
+        return incidents;
+      } else {
+        console.error('Failed to retrieve incidents. Status:', response.status);
+      }
+    } catch (error) {
+      console.error('Error retrieving incidents:', error);
+    }
+  }
+
+  async function getEquipmentDetails(incidentID, sort_by, display) {
+    const queryParams = new URLSearchParams({
+      incidentID,
+      sort_by,
+      display: display.join(',') // Convert array to comma-separated string
+    });
+  
+    try {
+      const response = await fetch(`/municipal/equipment?${queryParams}`, {
+        method: 'GET',
+        // Include headers if necessary
+      });
+  
+      if (response.ok) {
+        const equipmentDetails = await response.json();
+        console.log('Equipment details retrieved:', equipmentDetails);
+        return equipmentDetails;
+      } else {
+        console.error('Failed to retrieve equipment details. Status:', response.status);
+      }
+    } catch (error) {
+      console.error('Error retrieving equipment details:', error);
+    }
+  }
+  
 
 async function insertDemotable(id, name) {
     return await withOracleDB(async (connection) => {
