0942c61f306c083f31d9c2c000d5b1acfa3c403a
bc2003 <bc2003@students.cs.ubc.ca>
2023-11-26 00:25:03 -0800

    Bryan-queries I

--- sql_scripts/queries.sql
+++ sql_scripts/queries.sql
@@ -1,29 +1,49 @@
--- INSERT QUERY -> (action)
+-- INSERT QUERY -> Insert a new tuple in Department, also triggers new tuple in Location (due to foreign key)
 -- Query Implementation Reference: (file + directory + line)
-INSERT INTO ReportedBy (
-    incidentID,
-    email,
-    dateReported
+INSERT INTO Department (
+    branchID,
+    specialty,
+    locatedAtAddress
 ) VALUES (
-   :incidentID,
-   :email,
-   :dateReported
+    :branchID,
+    :specialty,
+    :locatedAtAddress
 );
 
--- DELETE QUERY -> (action)
+-- DELETE QUERY -> Delete a tuple from IncidentStatus (triggers ON DELETE CASCADE for IncidentInfo)
 -- Query Implementation Reference: (file + directory + line)
+DELETE FROM IncidentStatus I
+WHERE I.description = :description;
 
--- UPDATE QUERY -> (action)
+-- UPDATE QUERY -> Update an Incident (IncidentInfo) with a new date
 -- Query Implementation Reference: (file + directory + line)
+UPDATE IncidentInfo
+SET dateIncident = :date
+WHERE incidentID = :incidentID;
 
--- SELECTION QUERY -> (action)
+-- SELECTION QUERY -> Select incidents whether they occur before, on, or after the user-inputted date
 -- Query Implementation Reference: (file + directory + line)
+SELECT II.incidentID, II.dateIncident
+FROM IncidentInfo II
+WHERE dateIncident < TO_DATE(:date, 'DD-MON-YYYY');
+
+SELECT II.incidentID, II.dateIncident
+FROM IncidentInfo II
+WHERE dateIncident = TO_DATE(:date, 'DD-MON-YYYY');
+
+SELECT II.incidentID, II.dateIncident
+FROM IncidentInfo II
+WHERE dateIncident > TO_DATE(:date, 'DD-MON-YYYY');
 
 -- PROJECTION QUERY -> (action)
 -- Query Implementation Reference: (file + directory + line)
 
--- JOIN QUERY -> (action)
+-- JOIN QUERY -> Join on IncidentInfo and IncidentStatus so we can see the current status of incidents after a certain date
 -- Query Implementation Reference: (file + directory + line)
+SELECT *
+FROM IncidentInfo info, IncidentStatus stat
+WHERE info.dateIncident > TO_DATE(:date, 'DD-MON-YYYY') AND info.description = stat.description
+-- NOTE: NEED TO ACTUALLY GET USER INPUTTED DATE TO FILTER OUT INCIDENTS THAT HAVE OCCURRED SINCE USER-INPUTTED DATE
 
 -- AGGREGATION w/ GROUP BY QUERY -> (action)
 -- Query Implementation Reference: (file + directory + line)
