9c51d2d66f4f699ed8742e0a42b9e3a26f75fd4f
munce <munce@students.cs.ubc.ca>
2023-11-28 21:07:21 -0800

    Move all police scripts to police.js
    This helps with preventing bugs that happen with the initialization and
    stuff from scripts.js not existing in police.html

--- src/public/police.js
+++ src/public/police.js
@@ -32,6 +32,151 @@ function refreshTable(params) {
     });
 }
 
+async function getEquipment(event) {
+    event.preventDefault();
+    console.log("THIS GETS PRINTED")
+    const email = document.getElementById("IncidentID").value;
+    const response = await fetch('/municipal/equipment', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            deleteID: email,
+        })
+    });
+
+    const responseStatus = await response.status;
+    const messageElement = document.getElementById('getEquipment');
+
+    if (responseStatus === 200) {
+        messageElement.textContent = `The equipments are ${responseData.ID}, ${responseData.type},  ${responseData.weight}, ${responseData.color}`;
+    } else {
+        messageElement.textContent = `There was an error`;
+    }
+}
+
+async function deleteProfessionalID(event) {
+    event.preventDefault();
+    console.log("THIS GETS PRINTED")
+    const deleteID = document.getElementById("delete_prof").value;
+    const response = await fetch('/municipal/incident', {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            deleteID: deleteID,
+        })
+    });
+
+    const responseStatus = await response.status;
+    const messageElement = document.getElementById('deleted_Incident');
+
+    if (responseStatus === 200) {
+        messageElement.textContent = `The incident was deleted`;
+    } else {
+        messageElement.textContent = `There was an error`;
+    }
+}
+
+async function addProfessionalReporter(event) {
+    event.preventDefault();
+    console.log("THIS GETS PRINTED")
+    const professionalID = document.getElementById("ProfessionalreporterID").value;
+    const dept = document.getElementById("departmentNumber").value;
+    const name = document.getElementById("ResponderName").value;
+    const occupation = document.getElementById("Occupation").value;
+
+    const response = await fetch('/municipal/responder', {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            professionalID: professionalID,
+            dept: dept,
+            name: name,
+            occupation: occupation
+        })
+    });
+
+    const responseStatus = await response.status;
+    const messageElement = document.getElementById('addProfessionalReporter');
+
+    if (responseStatus === 200) {
+        messageElement.textContent = `Added the responder`;
+    } else {
+        messageElement.textContent = `There was an error`;
+    }
+}
+
+
+async function ViewNeighbourHood(event) {
+    event.preventDefault();
+    fetch('/municipal/neighbourhood/incidents')
+        .then(response => response.json())
+        .then(data => {
+            const resultsContainer = document.getElementById('results');
+            resultsContainer.innerHTML = '';
+            data.forEach(item => {
+                resultsContainer.innerHTML += `<p>${item[0]}: ${item[1]}</p>`;
+            });
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+async function ViewEquipmentWeights(event) {
+    event.preventDefault();
+    fetch('/municipal/neighbourhood/weight')
+        .then(response => response.json())
+        .then(data => {
+            const resultsContainer = document.getElementById('results');
+            resultsContainer.innerHTML = '';
+            data.forEach(item => {
+                resultsContainer.innerHTML += `<p>${item[0]}: ${item[1]}</p>`;
+            });
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+async function ViewNeighbourPolice(event) {
+    event.preventDefault();
+    fetch('/municipal/neighbourhood/police')
+        .then(response => response.json())
+        .then(data => {
+            const resultsContainer = document.getElementById('results');
+            resultsContainer.innerHTML = '';
+            data.forEach(item => {
+                resultsContainer.innerHTML += `<p>${item[0]}: ${item[1]}</p>`;
+            });
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+async function ViewOutStanding(event) {
+    event.preventDefault();
+    fetch('/municipal/neighbourhood/outstanding')
+        .then(response => response.json())
+        .then(data => {
+            const resultsContainer = document.getElementById('results');
+            resultsContainer.innerHTML = '';
+            data.forEach(item => {
+                resultsContainer.innerHTML += `<p>${item[0]}: ${item[1]}</p>`;
+            });
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+
 window.onload = () => {
     refreshTable(DEFAULT_PARAMS);
+    document.getElementById("getEquipment").addEventListener("submit", getEquipment, true);
+    document.getElementById("delete_prof").addEventListener("submit", deleteProfessionalID, true);
+    document.getElementById("addProfessionalReporter").addEventListener("submit", addProfessionalReporter, true);
+    document.getElementById("NeightbourHoodReports").addEventListener("click", ViewNeighbourHood, true);
+    document.getElementById("EquipmentWeights").addEventListener("click", ViewEquipmentWeights, true);
+    document.getElementById("FireStations").addEventListener("click", ViewNeighbourPolice, true);
+    document.getElementById("NoFireStations").addEventListener("click", ViewOutStanding, true);
+
 };
--- src/public/scripts.js
+++ src/public/scripts.js
@@ -178,37 +178,6 @@ async function addReporter(event) {
     }
 }
 
-async function addProfessionalReporter(event) {
-    event.preventDefault();
-    console.log("THIS GETS PRINTED")
-    const professionalID = document.getElementById("ProfessionalreporterID").value;
-    const dept = document.getElementById("departmentNumber").value;
-    const name = document.getElementById("ResponderName").value;
-    const occupation = document.getElementById("Occupation").value;
-
-    const response = await fetch('/municipal/responder', {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            professionalID: professionalID,
-            dept: dept,
-            name: name,
-            occupation: occupation
-        })
-    });
-
-    const responseStatus = await response.status;
-    const messageElement = document.getElementById('addProfessionalReporter');
-
-    if (responseStatus === 200) {
-        messageElement.textContent = `Added the responder`;
-    } else {
-        messageElement.textContent = `There was an error`;
-    }
-}
-
 async function deleteIncident(event) {
     event.preventDefault();
     console.log("THIS GETS PRINTED")
@@ -233,29 +202,7 @@ async function deleteIncident(event) {
     }
 }
 
-async function deleteProfessionalID(event) {
-    event.preventDefault();
-    console.log("THIS GETS PRINTED")
-    const deleteID = document.getElementById("delete_prof").value;
-    const response = await fetch('/municipal/incident', {
-        method: 'DELETE',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            deleteID: deleteID,
-        })
-    });
 
-    const responseStatus = await response.status;
-    const messageElement = document.getElementById('deleted_Incident');
-
-    if (responseStatus === 200) {
-        messageElement.textContent = `The incident was deleted`;
-    } else {
-        messageElement.textContent = `There was an error`;
-    }
-}
 
 async function getReporter(event) {
     event.preventDefault();
@@ -281,53 +228,7 @@ async function getReporter(event) {
     }
 }
 
-async function getEquipment(event) {
-    event.preventDefault();
-    console.log("THIS GETS PRINTED")
-    const email = document.getElementById("IncidentID").value;
-    const response = await fetch('/municipal/equipment', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            deleteID: email,
-        })
-    });
 
-    const responseStatus = await response.status;
-    const messageElement = document.getElementById('getEquipment');
-
-    if (responseStatus === 200) {
-        messageElement.textContent = `The equipments are ${responseData.ID}, ${responseData.type},  ${responseData.weight}, ${responseData.color}`;
-    } else {
-        messageElement.textContent = `There was an error`;
-    }
-}
-
-async function getProfessionalIncidents(event) {
-    event.preventDefault();
-    console.log("THIS GETS PRINTED")
-    const email = document.getElementById("IncidentID").value;
-    const response = await fetch('/municipal/incidents', {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            deleteID: email,
-        })
-    });
-
-    const responseStatus = await response.status;
-    const messageElement = document.getElementById('getReporterDetails');
-
-    if (responseStatus === 200) {
-        messageElement.textContent = `Added reporter Details are ${responseData.name}, ${responseData.address},  ${responseData.phoneNumber}`;
-    } else {
-        messageElement.textContent = `There was an error`;
-    }
-}
 
 
 
@@ -373,61 +274,7 @@ async function addInvolvedWitness(event) {
 }
 
 
-async function ViewNeighbourHood(event) {
-    event.preventDefault();
-    fetch('/municipal/neighbourhood/incidents')
-        .then(response => response.json())
-        .then(data => {
-            const resultsContainer = document.getElementById('results');
-            resultsContainer.innerHTML = '';
-            data.forEach(item => {
-                resultsContainer.innerHTML += `<p>${item[0]}: ${item[1]}</p>`;
-            });
-        })
-        .catch(error => console.error('Error:', error));
-}
 
-async function ViewEquipmentWeights(event) {
-    event.preventDefault();
-    fetch('/municipal/neighbourhood/weight')
-        .then(response => response.json())
-        .then(data => {
-            const resultsContainer = document.getElementById('results');
-            resultsContainer.innerHTML = '';
-            data.forEach(item => {
-                resultsContainer.innerHTML += `<p>${item[0]}: ${item[1]}</p>`;
-            });
-        })
-        .catch(error => console.error('Error:', error));
-}
-
-async function ViewNeighbourPolice(event) {
-    event.preventDefault();
-    fetch('/municipal/neighbourhood/police')
-        .then(response => response.json())
-        .then(data => {
-            const resultsContainer = document.getElementById('results');
-            resultsContainer.innerHTML = '';
-            data.forEach(item => {
-                resultsContainer.innerHTML += `<p>${item[0]}: ${item[1]}</p>`;
-            });
-        })
-        .catch(error => console.error('Error:', error));
-}
-
-async function ViewOutStanding(event) {
-    event.preventDefault();
-    fetch('/municipal/neighbourhood/outstanding')
-        .then(response => response.json())
-        .then(data => {
-            const resultsContainer = document.getElementById('results');
-            resultsContainer.innerHTML = '';
-            data.forEach(item => {
-                resultsContainer.innerHTML += `<p>${item[0]}: ${item[1]}</p>`;
-            });
-        })
-        .catch(error => console.error('Error:', error));
-}
 
 
 
@@ -450,13 +297,5 @@ window.onload = function() {
     document.getElementById("addIncident").addEventListener("submit", addIncident,true);
     document.getElementById("deleteIncident").addEventListener("submit", deleteIncident, true);
     document.getElementById("getReporterDetailsForm").addEventListener("submit", getReporter, true);
-    document.getElementById("getIncidents").addEventListener("submit", getProfessionalIncidents, true);
-    document.getElementById("getEquipment").addEventListener("submit", getEquipment, true);
-    document.getElementById("delete_prof").addEventListener("submit", deleteProfessionalID, true);
-    document.getElementById("addProfessionalReporter").addEventListener("submit", addProfessionalReporter, true);
-    document.getElementById("NeightbourHoodReports").addEventListener("click", ViewNeighbourHood, true);
-    document.getElementById("EquipmentWeights").addEventListener("click", ViewEquipmentWeights, true);
-    document.getElementById("FireStations").addEventListener("click", ViewNeighbourPolice, true);
-    document.getElementById("NoFireStations").addEventListener("click", ViewOutStanding, true);
 
 };
