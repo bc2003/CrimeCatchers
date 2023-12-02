39bb34f4957c6ee1a693ea4f6ca6bf5c82d04afa
bc2003 <bc2003@students.cs.ubc.ca>
2023-12-01 21:43:28 -0800

    bryan push first

--- sql_scripts/queries.sql
+++ sql_scripts/queries.sql
@@ -48,14 +48,14 @@ FROM IncidentInfo info, IncidentStatus stat
 WHERE info.dateIncident > TO_DATE(:date, 'DD-MON-YYYY') AND info.description = stat.description;
 
 -- AGGREGATION w/ GROUP BY QUERY -> Count the number of incidents in each neighbourhood
--- Query Implementation Reference: appService.js in (), starting at line
+-- Query Implementation Reference: appService.js in getIncidentAggregation(), starting at line
 SELECT L.neighbourhood, COUNT(*) AS TotalIncidents
 FROM OccurredAt OA, Location L
 WHERE OA.address = L.address
 GROUP BY L.neighbourhood;
 
 -- AGGREGATION w/ HAVING QUERY -> Return the total weight of equipment owned by each department with an inventory > 1
--- Query Implementation Reference: appService.js in (), starting at line
+-- Query Implementation Reference: appService.js in getWeightAggregation(), starting at line
 SELECT item.belongsToBranchID, SUM(info.weight) AS EquipmentWeight
 FROM EquipmentInfo info, EquipmentItem item
 WHERE info.type = item.type
@@ -63,7 +63,7 @@ GROUP BY item.belongsToBranchID
 HAVING COUNT(*) > 1;
 
 -- NESTED AGGREGATION w/ GROUP BY QUERY -> Return the neighbourhoods with more police stations than fire stations
--- Query Implementation Reference: appService.js in (), starting at line
+-- Query Implementation Reference: appService.js in getPoliceCalculation(), starting at line
 SELECT L1.neighbourhood
 FROM Department D1, Location L1
 WHERE D1.locatedAtAddress = L1.address and D1.specialty = 'police'
@@ -75,7 +75,7 @@ HAVING COUNT(*) > (
 );
 
 -- DIVISION QUERY -> Return all the neighbourhoods which had an incident, but does not have any first responder departments
--- Query Implementation Reference: appService.js in (), starting at line
+-- Query Implementation Reference: appService.js in getOutstandingCalculation(), starting at line
 SELECT L.neighbourhood
 FROM OccurredAt OA, Location L
 WHERE OA.address = L.address
