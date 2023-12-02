b7ce413e62152d9ec91adba9ecf13c7bb25cbed6
munce <munce@students.cs.ubc.ca>
2023-12-01 21:34:50 -0800

    update selection

--- sql_scripts/queries.sql
+++ sql_scripts/queries.sql
@@ -20,17 +20,9 @@ WHERE incidentID = :incidentID;
 
 -- SELECTION QUERY -> Select incidents based on who the user inputted as the email
 -- Query Implementation Reference: appService.js in getIncidents(), starting at line
-SELECT II.incidentID, II.dateIncident
-FROM IncidentInfo II
-WHERE dateIncident < TO_DATE(:date, 'DD-MON-YYYY');
-
-SELECT II.incidentID, II.dateIncident
-FROM IncidentInfo II
-WHERE dateIncident = TO_DATE(:date, 'DD-MON-YYYY');
-
-SELECT II.incidentID, II.dateIncident
-FROM IncidentInfo II
-WHERE dateIncident > TO_DATE(:date, 'DD-MON-YYYY');
+SELECT i.incidentID, i.description, i.dateIncident, s.statusValue
+FROM IncidentInfo i, IncidentStatus s, ReportedBy b
+WHERE i.description = s.description AND b.incidentID = i.incidentID AND b.email = :email
 
 -- PROJECTION QUERY -> allows user to choose a few columns to display
 -- Query Implementation Reference: appService.js in getIncidents(), starting at line
