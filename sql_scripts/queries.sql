420895fdbf59e0d84c9194dbe82090f4f2448df9
bc2003 <bc2003@students.cs.ubc.ca>
2023-12-01 20:00:00 -0800

    rough lines

--- sql_scripts/queries.sql
+++ sql_scripts/queries.sql
@@ -1,5 +1,5 @@
 -- INSERT QUERY -> Insert a new tuple in IncidentInfo, also triggers new tuple in IncidentStatus (due to foreign key)
--- Query Implementation Reference: (file + directory + line)
+-- Query Implementation Reference: appService.js in createIncident(), starting at line XXX
 INSERT INTO IncidentInfo (
     incidentID,
     dateIncident,
@@ -11,18 +11,18 @@ INSERT INTO IncidentInfo (
 );
 
 -- DELETE QUERY -> Delete a tuple from IncidentStatus (triggers ON DELETE SET NULL for IncidentInfo)
--- Query Implementation Reference: (file + directory + line)
+-- Query Implementation Reference: appService.js in updateIncident(), starting at line XXX
 DELETE FROM IncidentStatus I
 WHERE I.description = :description;
 
 -- UPDATE QUERY -> Update an Incident (IncidentInfo relation) with a new date
--- Query Implementation Reference: (file + directory + line)
+-- Query Implementation Reference: appService.js in updateIncident(), starting at line
 UPDATE IncidentInfo
 SET dateIncident = :date
 WHERE incidentID = :incidentID;
 
 -- SELECTION QUERY -> Select incidents whether they occur before, on, or after the user-inputted date
--- Query Implementation Reference: (file + directory + line)
+-- Query Implementation Reference: appService.js in getIncidents(), starting at line
 SELECT II.incidentID, II.dateIncident
 FROM IncidentInfo II
 WHERE dateIncident < TO_DATE(:date, 'DD-MON-YYYY');
@@ -35,26 +35,26 @@ SELECT II.incidentID, II.dateIncident
 FROM IncidentInfo II
 WHERE dateIncident > TO_DATE(:date, 'DD-MON-YYYY');
 
--- PROJECTION QUERY -> allows user to choose just 3 columns to display
--- Query Implementation Reference: (file + directory + line)
+-- PROJECTION QUERY -> allows user to choose a few columns to display
+-- Query Implementation Reference: appService.js in getIncidents(), starting at line
 SELECT :col1, :col2, :col3
 FROM :tableName;
 
 -- JOIN QUERY -> Join on IncidentInfo and IncidentStatus so we can see the current status of incidents after a certain date
--- Query Implementation Reference: (file + directory + line)
+-- Query Implementation Reference: appService.js in getIncidents(), starting at line
 SELECT *
 FROM IncidentInfo info, IncidentStatus stat
 WHERE info.dateIncident > TO_DATE(:date, 'DD-MON-YYYY') AND info.description = stat.description;
 
 -- AGGREGATION w/ GROUP BY QUERY -> Count the number of incidents in each neighbourhood
--- Query Implementation Reference: (file + directory + line)
+-- Query Implementation Reference: appService.js in (), starting at line
 SELECT L.neighbourhood, COUNT(*) AS TotalIncidents
 FROM OccurredAt OA, Location L
 WHERE OA.address = L.address
 GROUP BY L.neighbourhood;
 
 -- AGGREGATION w/ HAVING QUERY -> Return the total weight of equipment owned by each department with an inventory > 1
--- Query Implementation Reference: (file + directory + line)
+-- Query Implementation Reference: appService.js in (), starting at line
 SELECT item.belongsToBranchID, SUM(info.weight) AS EquipmentWeight
 FROM EquipmentInfo info, EquipmentItem item
 WHERE info.type = item.type
@@ -62,7 +62,7 @@ GROUP BY item.belongsToBranchID
 HAVING COUNT(*) > 1;
 
 -- NESTED AGGREGATION w/ GROUP BY QUERY -> Return the neighbourhoods with more police stations than fire stations
--- Query Implementation Reference: (file + directory + line)
+-- Query Implementation Reference: appService.js in (), starting at line
 SELECT L1.neighbourhood
 FROM Department D1, Location L1
 WHERE D1.locatedAtAddress = L1.address and D1.specialty = 'police'
@@ -74,7 +74,7 @@ HAVING COUNT(*) > (
 );
 
 -- DIVISION QUERY -> Return all the neighbourhoods which had an incident, but does not have any first responder departments
--- Query Implementation Reference: (file + directory + line)
+-- Query Implementation Reference: appService.js in (), starting at line
 SELECT L.neighbourhood
 FROM OccurredAt OA, Location L
 WHERE OA.address = L.address
